1d79ca668f861b77b3a4da9fe5bf1752dc724f58
Adel Abderrahmane <adel2009dz@gmail.com>
2018-07-31 10:38:38 +0200

    Add files via upload

--- wp-login.php
+++ wp-login.php
@@ -9,7 +9,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Farid Blog &lsaquo; Log In</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='/a7adel.github.io/farid-website/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.7' type='text/css' media='all' />
+<link rel='stylesheet' href='/farid-website/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.7' type='text/css' media='all' />
 <meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
 		</head>
@@ -17,7 +17,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Farid Blog</a></h1>
 	
-<form name="loginform" id="loginform" action="/a7adel.github.io/farid-website/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="/farid-website/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -35,7 +35,7 @@
 </form>
 
 <p id="nav">
-	<a href="/a7adel.github.io/farid-website/wp-login.php?action=lostpassword">Lost your password?</a>
+	<a href="/farid-website/wp-login.php?action=lostpassword">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -59,7 +59,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="/a7adel.github.io/farid-website/">&larr; Back to Farid Blog</a></p>
+	<p id="backtoblog"><a href="/farid-website/">&larr; Back to Farid Blog</a></p>
 	
 	</div>
 
